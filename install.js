5eceb00016eb2170a1c5e6cbbc7d380a0ed1172e
Blazh <iblazevi@gmail.com>
2018-09-02 17:53:02 +0200

    Try with force

--- install.js
+++ install.js
@@ -3,8 +3,8 @@
 
 const shell = require('shelljs');
 //shell.exec(comandToExecute, {silent:true}).stdout;
-shell.exec('apt-get update')
-shell.exec('apt-get install mpc')
-shell.exec('apt-get install python')
+shell.exec('apt-get --yes --force-yes update')
+shell.exec('apt-get --yes --force-yes install mpc')
+shell.exec('apt-get --yes --force-yes install python')
 shell.exec('echo "export PORT=80" >> ~/.bashrc')
 shell.exec('sed -i -e \'s/DietPi/radix/g\' /etc/hosts')
