78bffaf5aab5f798a6048a4ba6c001b549525be6
Blaz <i.blazevic@palfinger.com>
2019-06-24 11:55:47 +0200

    Add volume slider

---
+++ www/precache-manifest.730a1905673c52d9447dafb811965a16.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8dc9fdf180f8dd9559503767d5603678",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ebf2868b98a3ba469a63",
+    "url": "/static/css/2.509658c0.chunk.css"
+  },
+  {
+    "revision": "eaaa15bd46202d7323ef",
+    "url": "/static/css/main.87b09e0f.chunk.css"
+  },
+  {
+    "revision": "ebf2868b98a3ba469a63",
+    "url": "/static/js/2.c08589f8.chunk.js"
+  },
+  {
+    "revision": "eaaa15bd46202d7323ef",
+    "url": "/static/js/main.f188410e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- www/service-worker.js
+++ www/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6cf9bcdf986f64a65cd011a04de33677.js"
+  "/precache-manifest.730a1905673c52d9447dafb811965a16.js"
 );
 
 self.addEventListener('message', (event) => {
