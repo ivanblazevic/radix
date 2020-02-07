e77ad40d14687ce85018e62e9faa0454d7fa1213
Ivan Blazevic <ivan.blazevic@gmail.com>
2020-02-07 22:44:55 +0100

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,122 +1,128 @@
-import * as fs from 'fs';
-import * as nconf from 'nconf';
-import * as path from 'path';
-import * as request from 'request';
-import { Observable } from 'rxjs/Observable';
+import * as fs from "fs";
+import * as nconf from "nconf";
+import * as path from "path";
+import * as request from "request";
+import { Observable } from "rxjs/Observable";
 
 export class Config {
-    private configPath = path.join('./config.json');
-    private packagePath = path.join(__dirname, './../package.json');
-    private defaultConfig = {
-        "mixer": "PCM",
-        "executor": "mpc",
-        "url": "http://alternativefm.cast.addradio.de/alternativefm/simulcast/high/stream.mp3",
-        "title": "AlternativeFM",
-        "volume": 50
-    }
+  private configPath = path.join("./config.json");
+  private packagePath = path.join(__dirname, "./../package.json");
+  private defaultConfig = {
+    mixer: "PCM",
+    executor: "mpc",
+    url:
+      "http://alternativefm.cast.addradio.de/alternativefm/simulcast/high/stream.mp3",
+    title: "AlternativeFM",
+    volume: 50,
+    ws: "localhost"
+  };
 
-    constructor() {
-        if (!fs.existsSync(this.configPath)) {
-            fs.writeFileSync(this.configPath, JSON.stringify(this.defaultConfig));
+  private ws;
+
+  constructor() {
+    if (!fs.existsSync(this.configPath)) {
+      fs.writeFileSync(this.configPath, JSON.stringify(this.defaultConfig));
+    }
+    nconf
+      .argv()
+      .env()
+      .file({ file: this.configPath });
+  }
+
+  get(param: string): any {
+    return nconf.get(param);
+  }
+
+  getVersion(): string {
+    var content = fs.readFileSync(this.packagePath, "utf8");
+    var o = JSON.parse(content);
+    return o.version;
+  }
+
+  getAvailableVersion(): Observable<string> {
+    return new Observable(observer => {
+      request.get("https://registry.npmjs.org/radix-player", function(
+        error,
+        response,
+        body
+      ) {
+        if (!error && response.statusCode == 200) {
+          const b = JSON.parse(body);
+          observer.next(b["dist-tags"].latest);
+        } else {
+          observer.next("Executing: ");
         }
-        nconf
-            .argv()
-            .env()
-            .file({ file: this.configPath });
-    }
+        observer.complete();
+      });
+    });
+  }
 
-    get(param: string): any {
-        return nconf.get(param);
-    }
+  getMixer(): string {
+    return this.get("mixer") || "PCM";
+  }
 
-    getVersion(): string {
-        var content = fs.readFileSync(this.packagePath, 'utf8');
-        var o = JSON.parse(content);
-        return o.version;
+  getVolume(): number {
+    const volume = this.get("volume");
+    if (!volume) {
+      return 50;
     }
+    return +volume;
+  }
 
-    getAvailableVersion(): Observable<string> {
-        return new Observable(observer => {
-            request.get('https://registry.npmjs.org/radix-player',
-                function (error, response, body) {
-                    if (!error && response.statusCode == 200) {
-                        const b = JSON.parse(body);
-                        observer.next(b["dist-tags"].latest);
-                    } else {
-                        observer.next("Executing: ");
-                    }
-                    observer.complete();
-                }
-            );
-        })
-    }
+  setVolume(volume: number): void {
+    this.save("volume", volume.toString());
+  }
 
-    getMixer(): string {
-        return this.get('mixer') || 'PCM';
-    }
+  getStreamingUrl(): string {
+    return this.get("url");
+  }
 
-    getVolume(): number {
-        const volume = this.get('volume');
-        if (!volume) {
-            return 50;
-        }
-        return +volume;
-    }
+  setStreamingUrl(url: string): void {
+    this.save("url", url);
+  }
 
-    setVolume(volume: number): void {
-        this.save('volume', volume.toString());
-    }
+  getTitle(): string {
+    return this.get("title");
+  }
 
-    getStreamingUrl(): string {
-        return this.get('url');
-    }
+  setTitle(title: string): void {
+    this.save("title", title);
+    this.ws.send(title);
+  }
 
-    setStreamingUrl(url: string): void {
-        this.save('url', url);
-    }
+  getGoogleUsername(): string {
+    return this.get("google_username");
+  }
 
-    getTitle(): string {
-        return this.get('title');
-    }
+  setGoogleUsername(value: string): void {
+    this.save("google_username", value);
+  }
 
-    setTitle(title: string): void {
-        this.save('title', title);
-    }
+  getGooglePassword(): string {
+    return this.get("google_password");
+  }
 
-    getGoogleUsername(): string {
-        return this.get('google_username');
-    }
+  setGooglePassword(value: string): void {
+    this.save("google_password", value);
+  }
 
-    setGoogleUsername(value: string): void {
-        this.save('google_username', value);
-    }
+  getDirbleToken(): string {
+    return this.get("dirble_token");
+  }
 
-    getGooglePassword(): string {
-        return this.get('google_password');
-    }
+  setDirbleToken(value: string): void {
+    this.save("dirble_token", value);
+  }
 
-    setGooglePassword(value: string): void {
-        this.save('google_password', value);
-    }
+  isMpv(): boolean {
+    return this.get("executor") === "mpv";
+  }
 
-    getDirbleToken(): string {
-        return this.get('dirble_token');
-    }
-
-    setDirbleToken(value: string): void {
-        this.save('dirble_token', value);
-    }
-
-    isMpv(): boolean {
-        return this.get('executor') === "mpv";
-    }
-
-    private save(param: string, value: string) {
-        nconf.set(param, value);
-        nconf.save(err => {
-            console.log(err);
-            fs.readFile(this.configPath, function (err, data) { });
-        });
-    }
-
+  private save(param: string, value: string) {
+    nconf.set(param, value);
+    nconf.save(err => {
+      console.log(err);
+      fs.readFile(this.configPath, function(err, data) {});
+    });
+  }
 }
