22e4595667857aec3800d9896039ccaf77a54148
Ivan Blazevic <ivan.blazevic@gmail.com>
2020-02-07 22:42:34 +0100

    Add support for 7digit display

---
+++ src/ws.ts
@@ -0,0 +1,34 @@
+import * as WebSocket from "ws";
+
+export class WebSocketHelper {
+  private url: string;
+
+  constructor(private wsHost: string, private title: string) {
+    this.url = wsHost;
+
+    setTimeout(() => {
+      this.send("radix");
+    }, 2000);
+
+    setTimeout(() => {
+      this.send(title);
+    }, 8000);
+  }
+
+  send = (text: string) => {
+    // TODO: reuse ws instance
+    const socket = new WebSocket("ws://" + this.url + ":8765");
+
+    socket.on("error", function(err) {
+      console.log(err);
+    });
+
+    socket.on("open", () => {
+      try {
+        socket.send(text.substring(0, 8).toUpperCase());
+      } catch (error) {
+        console.log(error);
+      }
+    });
+  };
+}
